7d5f097e6c4e62f93c22ee19c0ab01ab3eb2a8c4
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-03 21:54:06 +0400

    vitom ar shemshlia meore subtask?

--- src/pgdp/carol/CarolPfadfinder.java
+++ src/pgdp/carol/CarolPfadfinder.java
@@ -24,7 +24,37 @@ public class CarolPfadfinder {
 	}
 	static int getMinimalStepsAndTurns(int x, int y, int direction, int findX, int findY)
 	{
-        return 0;
+		int result=0;
+		if(Math.abs(findY-y)>0 && Math.abs(findX-x)>0)
+		{
+			result+=Math.abs(findX-x)+1+Math.abs(findY-y);
+			if(findX > x && direction==2) result++;
+			else if (findX <x && direction ==0) result++;
+			if(findY > y && direction==3) result++;
+			else if (findY <y && direction ==1) result++;
+		}
+		else if(Math.abs(findX-x)>0)
+		{
+			result+=Math.abs(findX-x)+1;
+			if(direction%2==0)
+			{
+				if(findX > x && direction==0) result--;
+				else if (findX <x && direction ==2) result--;
+				else result++;
+			}
+
+		}
+		else if(Math.abs(findY-y)>0)
+		{
+			result+=Math.abs(findY-y)+1;
+			if(direction%2==1)
+			{
+				if(findY > y && direction==1) result--;
+				else if (findY <y && direction ==3) result--;
+				else result++;
+			}
+		}
+		return result;
 	}
 	public static boolean findInstructions(int[][] playground, int x, int y, int direction, int blocks, int findX, int findY, char[] instructions){
 		return false;

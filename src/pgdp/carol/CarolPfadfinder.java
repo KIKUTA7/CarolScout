d100c469569cb4d86cc89a63ab9805611b23fbaf
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-04 16:16:16 +0400

    gh

--- src/pgdp/carol/CarolPfadfinder.java
+++ src/pgdp/carol/CarolPfadfinder.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class CarolPfadfinder {
     static boolean lastTurnsAreUseless(char[] instr, int filled) {
-                if(filled <2) return false;
+                if(filled <2 && filled>=0 ) return false;
         if (instr[filled - 1] == 'r' && instr[filled - 2] == 'l') return true;
         if (instr[filled - 1] == 'l' && instr[filled - 2] == 'r') return true;
         if (instr[filled - 1] == 'r' && instr[filled - 2] == 'r') return true;
@@ -75,7 +75,7 @@ public class CarolPfadfinder {
         return result;
     }
 
-  /*  public static boolean findInstructions(int[][] playground, int x, int y, int direction, int blocks, int findX, int findY, char[] instructions) {
+    public static boolean findInstructions(int[][] playground, int x, int y, int direction, int blocks, int findX, int findY, char[] instructions) {
 
         return false;
     }
@@ -85,7 +85,7 @@ public class CarolPfadfinder {
         return j;
 
     }
-*/
+
     public static void main(String[] args) {
         /*
          * You can change this main-Method as you want. This is not being tested.

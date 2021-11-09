0c9dce230653b1e0d8480c4079f6de38248253e4
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-11-09 17:10:08 +0400

    exla 30/30 iani 100% gvinda

--- src/pgdp/carol/CarolPfadfinder.java
+++ src/pgdp/carol/CarolPfadfinder.java
@@ -280,7 +280,8 @@ public class CarolPfadfinder {
 //
 //        }
         char [] instructions  = new char [1];
-        for (int i=0;i<=searchLimit;i++)
+        if(x==findX && y==findY) return new char [0];
+        for (int i=1;i<=searchLimit;i++)
         {
             instructions  = new char [i];
             if(findInstructions(playground,x,y,direction,blocks,findX,findY,instructions))
